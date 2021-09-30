0eb541e9cd85b8b0aeedfae1c33c47eaf99a9d66
Rafal Chlodnicki <rchl2k@gmail.com>
2021-09-30 12:01:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# typedi versions
+
+1.
+
+```sh
+npm i && npm run dev
+# or
+yarn && yarn dev
+```
+
+2. Open http://localhost:4000
+3. Make a query:
+
+```
+query {
+    album {
+        id
+    }
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import 'reflect-metadata';
+import { Container, Inject } from 'typedi';
+import { buildSchema, ObjectType, Field, Query, Resolver } from 'type-graphql';
+import { ApolloServer } from 'apollo-server';
+
+// --- Injected controlled ---
+
+class ApiController {}
+
+abstract class Controller {
+    @Inject('ApiController')
+    protected readonly api!: ApiController;
+
+    constructor(apiController: ApiController) {
+        this.api = apiController;
+    }
+}
+
+// --- Album resolver ---
+
+@ObjectType()
+export class Album {
+    @Field({ nullable: false })
+    public id!: number;
+}
+
+@Resolver(() => Album)
+export class AlbumController extends Controller {
+    @Query(() => Album, { name: 'album' })
+    public album(): Album {
+        return {
+            id: 666,
+        };
+    }
+}
+
+async function startServer(): Promise<void> {
+    Container.set('ApiController', new ApiController());
+
+    const schema = await buildSchema({
+        resolvers: [AlbumController],
+        emitSchemaFile: true,
+        // Used to inject `ApiController` into controller instances.
+        container: Container,
+        // Avoids warning when class-validator is not used in the code.
+        validate: false,
+        // All fields are nullable by default per webapi documentation.
+        nullableByDefault: true,
+    });
+
+    const server = new ApolloServer({
+        schema,
+        debug: true,
+        playground: true,
+    });
+
+    server.listen().then(({ url }) => {
+        console.info(`🚀 Server ready at ${url}`);
+    });
+}
+
+startServer();
